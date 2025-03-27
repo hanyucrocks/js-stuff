8d2e17233fbb58760af09d10d3e49ff56e28fabf
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-27 19:49:23 +0000

    lecture 10 done <3

---
+++ basics/lecture10.js
@@ -0,0 +1,20 @@
+// lecture 10 stack and heap memory, remember cpp? wahi cheez hai
+
+// Stack, Heap
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitve), Heap (Non Primitive)
+// stack me copy milega
+// heap me reference milega
+
+let mmhm = "what"
+let anothermm = mmhm
+anothermm = "dallas"
+console.log(mmhm);
+console.log(anothermm);
+
+let userOne = {
+    email :"how@google.com",
+    upi: "asd@ybl"
+}
