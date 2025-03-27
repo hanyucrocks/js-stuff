05a6237ae8f5e0b0625c761b50429868c3d3bcbd
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-27 19:09:43 +0000

    lecture 7 done

---
+++ basics/lecture7.js
@@ -0,0 +1,20 @@
+// operations in js
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue)
+
+let str1="hello"
+let str2=" jasmine"
+
+let str3=str1+str2
+
+console.log(str3);
+
+// console.log((3+4)*5%3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
