2a7213588cb4cd62f841c17b062ffc13e4335844
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-27 19:40:08 +0000

    lecture 9 done

---
+++ basics/lecture9.js
@@ -0,0 +1,40 @@
+// lecture 9 letsss go
+// Primitive data types
+
+/* 7 types
+String
+Number
+boolean
+null
+undefined
+symbol
+BigInt
+*/
+
+// Reference Data type, non primitive
+
+// Array, Objects, Functions
+
+// JavaScript is what? dynamic typed lang
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail;  // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const heroes = ["eminem", "drdre", "xbzitit"]
+let myObj = {
+    name: "dallas",
+    age : 26,
+}
+
+const myfunc = function(){
+    console.log("Hello boii");
+}
