fbdc8e5e24dab1937fb21ba4fc285e334ae1704d
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-27 18:31:41 +0000

    lecture 5 done

---
+++ basics/lecture5.js
@@ -0,0 +1,27 @@
+// lecture 5 lets go
+// data types
+"use strict"; // treat all JS code as newer version.
+
+// alert(3+3) // we are using NodeJs, not browser
+
+console.log(3+3)
+
+let name = "Okay"
+let age = 15
+let isLoggedIn = false
+
+// number -> 2^53 range
+// bigInt -> 2^90?
+// string '' or ""
+// bool t or f
+/*
+
+null -> standalone value
+symbol -> unique
+
+object
+
+*/
+
+console.log(typeof "agnish") // typeof will tell the type of the input.
+console.log(typeof undefined) // undefined
