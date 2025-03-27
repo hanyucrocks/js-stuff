c6963447237ed8535d1e47260e59e64f99edde94
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-27 19:23:26 +0000

    lecture 8 done

---
+++ basics/lecture8.js
@@ -0,0 +1,8 @@
+// lecture 8 lets goooo
+
+console.log(null > 0);
+console.log(undefined > 0);
+ // bc kya ho raha hai yaha
+
+ // === strictly check, data type bhi check karega
+ console.log("2"===3); // data type bhi check karega
