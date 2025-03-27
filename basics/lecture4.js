1d5eb2c1b81d94fdad7cc392807b33bd799bbca7
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-27 17:33:42 +0000

    lecture 4 done

---
+++ basics/lecture4.js
@@ -0,0 +1,22 @@
+const accountId = 786
+let accountEmail = "xys@gmail.com" // variable declare
+var accountPassword = "1223" // change kar sakta hu
+accountCity = "Toronto"
+let accountState; // isme undefined aaega
+//  accountId = 2
+
+console.log(accountId);
+
+accountEmail = "Dallas"
+accountPassword = "2123121"
+accountCity = "Canada"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// NEVER USE VAR. GTFO VARRRRRR....
+
+/*
+
+var use mat karna, cause of issue in block scope and functional scope.
+
+*/
