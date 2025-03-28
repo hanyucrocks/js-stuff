9ed86479d3c8028bf794d01e25af0c7c019d0ae0
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-28 11:57:25 +0000

    lecture 12 done

---
+++ basics/lecture12.js
@@ -0,0 +1,25 @@
+// lecture 12 letss goooo
+
+const score = 3000
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.948;
+console.log(otherNumber.toPrecision(3)); // 124
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN')); // 100, 000
+
+// ----------------------------- MATHS -----------------------------
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.3));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min) // abe ye 10 se upar aaega random mein
+
