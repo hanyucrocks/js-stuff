9b5c0d053cb865a17c5c5a5ca99be5d0b69e7518
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-28 18:25:41 +0000

    lecture 14 done

---
+++ basics/lecture14.js
@@ -0,0 +1,29 @@
+// lecture 14 lets goo
+
+// ARRAYS IN JS
+// part 2 basics
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myChars = ["Emma", "Adele"]
+const myArr2= new Array(1, 2, 3, 4);
+
+// console.log(myArr[1]);
+
+// // Array methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(2); // array ke start me aayega wo x.
+// console.log(myArr);
+// console.log(myArr.indexOf(1));
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1=myArr.slice(1, 3);
+console.log(myn1);
+console.log("B", myArr);
+
+// slice 1 2 
+// splice 1 2 3 splice me original me manipulate hota hai
