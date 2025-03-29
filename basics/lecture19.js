849ec35224b191b70b9561cba5fdfdc7b9248669
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-29 08:43:09 +0000

    lecture 19 done

---
+++ basics/lecture19.js
@@ -0,0 +1,23 @@
+// lecture 19 letsss go
+
+// functions and parameter
+
+function sayMyName(){
+    console.log("Heisenberg");
+}
+
+// sayMyName();
+
+function addTwoNumber(number1, number2){
+    return number1+number2;
+}
+// addTwoNumber(3, 1);
+
+// const result = addTwoNumber(3, 5)
+// console.log(result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Jasmine"))
