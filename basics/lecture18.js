e8339b57ff49dc52d8c69c12c88de7d035048696
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-29 07:20:05 +0000

    lecture 18 done

---
+++ basics/lecture18.js
@@ -0,0 +1,20 @@
+// lecture 18 letsss gooo
+
+// object destructure and JSON API
+
+const course = {
+    name:"js in hindi",
+    price: "999",
+    teacher:"hitesh"
+}
+
+const {teacher : tea} = course
+
+console.log(tea);
+
+// {
+//     "name":"amy",
+//     "coursename": "free",
+//     "price":"what"
+// }  this that json api shit
+
