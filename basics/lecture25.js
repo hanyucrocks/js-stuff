ce7115b59a5733326a4ab4db28a3f59df7bf1aea
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-03-31 07:20:35 +0000

    lecture 25 done

---
+++ basics/lecture25.js
@@ -0,0 +1,31 @@
+// lecture 25 letss gooo
+
+// important for interviews
+
+// call stack
+
+/*
+javascript execution context
+{} -> global EC (this)
+
+global execution context
+function execution context
+eval execution context
+{} -> Memory (creation phase)
+ execution phase
+
+ val = undefined
+ val2 = undefined hoga
+ add num -> definiton
+ result1 -> undefined
+ result2 -> undefined
+ 
+ phir executiion phase
+ val1 - 10
+ val2 - 5
+ add num -> new vairbale environment + execution thread
+
+ stack - last in first out.
+ 
+
+*/
