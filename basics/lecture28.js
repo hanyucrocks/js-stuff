e7aa546a95a1f855e1872d3738a5e885a8821f24
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-04-06 11:32:06 +0000

    lecture 28 done

---
+++ basics/lecture28.js
@@ -0,0 +1,22 @@
+// lecture 28
+
+// let i = 0;
+// while(i <= 10){
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2
+// }
+
+let bhm = ['emma', 'dallas']
+
+// let arr =  0
+// while(arr < bhm.length){
+//     console.log(`name is ${bhm[arr]}`);
+//     arr++;
+// }
+
+let score = 100
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
