c6c02ebde628961bbd24838c59a51b5fddce2ea9
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-21 15:46:10 +0000

    lecture 32 done

---
+++ basics/lecture32.js
@@ -0,0 +1,18 @@
+// lecture 32, DOM selectors NodeList and HTMLCollection
+// some stuff in the console tab of wikipedia
+
+/*
+
+
+
+Array.from(tempClassList)
+const myConvertedArray = Array.from(tempClassList);
+f
+
+
+
+
+
+
+
+*/
