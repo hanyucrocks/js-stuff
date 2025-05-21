0ed2affd7f96cf1330be39db62d7cf0324c9cb4c
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-21 14:40:19 +0000

    lecture 31 done

---
+++ basics/lecture31.js
@@ -0,0 +1,30 @@
+// lecture 31, DOM introduction
+// DOM LEARNING - document object model
+/*
+window -> doc -> html
+html has two childs
+1. head
+2. body
+
+head -> title -> text node
+head -> meta -> attribute
+
+body -> div -> attribute
+div has 2 childs
+
+1. h1 -> att && textnode
+2. paragraph -> textnode
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
