ba02585c6d519827cc20971e46ea34d36fde54a1
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-22 18:23:35 +0000

    lecture 41 done

---
+++ advanced_stuff/lecture41.js
@@ -0,0 +1,18 @@
+//fetch what is fetch
+/**
+ * 
+ * fetch something
+ * 1. datta
+ * 2. web browser / node
+ * network request
+ * 
+ * fetch -> network request on web browser / node -> fulfil or rejection  -> response which is global memory
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
