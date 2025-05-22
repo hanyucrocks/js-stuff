a8dd9061bf59ea6e794ac6c349713dd6676c06e0
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-05-22 07:01:52 +0000

    lecture 37 done

---
+++ basics/lecture37.js
@@ -0,0 +1,48 @@
+// async code
+/** 
+ * javascript -> synchronous, single threaded, slow language
+ * execution context -> execute one line of code at a time
+ * blocking code vs non blocking code
+ * block the code of program -> read file sync
+ * non blocking code - doesnt block execution
+ * blocking code is also good
+ * js engine - memory heap and call stack
+ * DOM API / Node
+ * ek drawing hai, usko dekh lena before interviews ig
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
